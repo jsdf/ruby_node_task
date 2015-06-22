31c48f4b30dbd6899803c35c28fdfdc97d8a2cfa
James Friend <james@jsdf.co>
2015-06-22 13:40:21 +1000

    allow task modules to be specified as absolute paths

--- spec/node_task_spec.rb
+++ spec/node_task_spec.rb
@@ -1,8 +1,11 @@
 require 'node_task'
 RSpec.describe NodeTask do
+  after(:each) do
+    NodeTask.release
+  end
+
   it "spawns a node server" do
     server = NodeTask.server
-    NodeTask.release
   end
 
   it "runs tasks" do
@@ -14,5 +17,12 @@ RSpec.describe NodeTask do
     result = t.run
     expect(result).not_to be_nil
     expect(result[:count]).to be(2)
+  end
+
+  it "runs tasks referenced by absolute paths" do
+    t = NodeTask.new File.expand_path('./lib/node_task/example/count.js')
+    result = t.run
+    expect(result).not_to be_nil
+    expect(result[:count]).to be(1)
   end
 end
